d6d50dff7ecba9d476e1d4b22ecf7da51a3652b9
Luneriane <luneriane@gmail.com>
2019-07-24 18:11:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,133 @@
+// Eléments html
+const people = document.getElementById('people');
+const previousTeam = document.getElementById('previousTeam');
+const howMany = document.getElementById('howMany');
+const howManyBtn = document.getElementById('howManyBtn');
+const newTeam = document.getElementById('newTeam');
+// Local Storage
+let previousData = JSON.parse(window.localStorage.getItem('previousTeams'));
+// JSON
+let originalData;
+// Copie temporaire du JSON
+let tempData = [];
+// Contenant des groupes
+let myGroups = new Array();
+// Présence de listes
+let hasLists = false;
+
+
+/* Récupération des datas JSON */
+window.onload = () => {
+    var request = new XMLHttpRequest();
+    request.open("get", "./data.json", true);
+    request.onreadystatechange = (e) => {
+        if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+            console.log(e.currentTarget.response)
+            originalData = JSON.parse(e.currentTarget.response);
+
+            howMany.setAttribute('max', Math.ceil(originalData.data.length / 2));
+            insertData(originalData);
+        }
+    };
+    request.send();
+}
+
+/* Intégration des datas dans la page */
+function insertData(data) {
+    let newUl = document.createElement('ul');
+    people.appendChild(newUl);
+
+    for (let person in data.data) {
+        let newLi = document.createElement('li');
+        newLi.innerText = data.data[person].name;
+        newUl.appendChild(newLi);
+    }
+}
+
+/* Données temporaires CHECKED*/
+function temporaryData (data) {
+    for (let person in data) {
+        tempData.push(data[person].name)
+    };
+}
+
+/* Remise à zéro */
+function cleanMyDatas(){
+    var myUls = document.querySelectorAll('#newTeam ul');
+    console.log(newTeam);
+    Array.prototype.forEach.call( myUls, function( node ) {
+        console.log(node)
+        node.parentNode.removeChild( node );
+    });
+    
+    tempData = [];
+    hasLists = false;
+}
+
+/* Nombre de groupes */
+function numberOfGroups(numb){
+    for (let i = 0; i < numb; i++){
+        var group = new Array();
+        myGroups.push(group);
+    }
+    console.log("groupes");
+    console.log(myGroups);
+}
+
+/* Constitution des groupes */
+function completingGroup(data){
+
+    let groupI = 0;
+    console.log(data);
+
+    for (let dataI in data) {
+        let randomI = Math.floor(Math.random()*data.length);
+        
+        myGroups[groupI].push(data[randomI]);
+        data.splice(randomI, 1);
+
+        groupI++;
+        if (groupI > myGroups.length-1) { groupI = 0 };
+
+        console.log(randomI);
+        console.log(data);
+    }
+    console.log(myGroups);
+}
+
+/* Affichage des groupes CHECKED*/
+function showingGroups(groups){
+    for (let group in groups) {
+        let newUl = document.createElement('ul');
+        newTeam.appendChild(newUl);
+
+        for (let person in groups[group]) {
+            let newLi = document.createElement('li');
+            newLi.innerText = groups[group][person];
+            newUl.appendChild(newLi);
+        }
+    }
+}
+
+/* Random Team */
+howManyBtn.onclick = () => {
+    let howManyGroups = Math.ceil(originalData.data.length / howMany.value);
+
+    /* Netoyage des listes */
+    if (hasLists) { cleanMyDatas() };
+
+    /* Stockage des données */
+    temporaryData(originalData.data);
+
+    /* Nombre de groupes */
+    numberOfGroups(howManyGroups);
+
+    /* Constitution des groupes */
+    completingGroup(tempData);
+
+    /* Afficher les groupes */
+    showingGroups(myGroups)
+
+    /* Présence de listes */
+    hasLists = true;
+}
